f38d91cd749c63ba139bdabb02f7272c89969858
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-07 05:33:48 +0900

    change: hello-worldを表示しました

--- src/main/java/portfolio/StudentManagement/StudentManagementApplication.java
+++ src/main/java/portfolio/StudentManagement/StudentManagementApplication.java
@@ -2,11 +2,19 @@ package portfolio.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "<h1>Hello, World!</h1>";
 	}
 }
