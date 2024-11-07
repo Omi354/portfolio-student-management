ee470e49519f001bc3eb9aa9928a151c4d3869b3
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-08 00:23:41 +0900

    feat: DBなしで動くWebアプリケーションを作成しました

--- src/main/java/portfolio/StudentManagement/StudentManagementApplication.java
+++ src/main/java/portfolio/StudentManagement/StudentManagementApplication.java
@@ -1,21 +1,57 @@
 package portfolio.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
+	private Map<String, String> studentInfoMap = new HashMap<>();
+
+	public StudentManagementApplication() {
+		studentInfoMap.put("name", "omi");
+		studentInfoMap.put("age", "92");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "<h1>Hello, World!</h1>";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return studentInfoMap.get("name") + " " + studentInfoMap.get("age") + "歳";
 	}
+
+	@GetMapping("/studentName")
+	public String getStudentName() {
+		return studentInfoMap.get("name");
+	}
+
+	@GetMapping("/studentAge")
+	public String getStudentAge() {
+		return studentInfoMap.get("age");
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfoMap(String name, String age) {
+		studentInfoMap.put("name", name);
+		studentInfoMap.put("age", age);
+	}
+
+	@PostMapping("/studentName")
+	public void setStudentName(String name) {
+		studentInfoMap.put("name", name);
+	}
+
+	@PostMapping("/studentAge")
+	public void setStudentAge(String age) {
+		studentInfoMap.put("age", age);
+	}
+
 }
