3bda127f98070da52911972e7bc5531b76c29c19
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-11 22:23:49 +0900

    add: リポジトリファイルにJavadocを記述しました

--- src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
+++ src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
@@ -5,8 +5,17 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import portfolio.StudentManagement.data.StudentCourse;
 
+/**
+ * 受講生コース情報を扱うリポジトリ
+ * 全件検索や単一条件での検索が行えるクラスです
+ */
 @Mapper
 public interface StudentCourseRepository {
+
+  /**
+   * 全件検索します
+   * @return 全件検索した受講生コースの一覧
+   */
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> selectAllStudentCourseList();
 }
--- src/main/java/portfolio/StudentManagement/repository/StudentRepository.java
+++ src/main/java/portfolio/StudentManagement/repository/StudentRepository.java
@@ -5,8 +5,16 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import portfolio.StudentManagement.data.Student;
 
+/**
+ * 受講生情報を扱うリポジトリ
+ * 全件検索や単一条件での検索が行えるクラスです
+ */
 @Mapper
 public interface StudentRepository {
+  /**
+   * 全件検索します
+   * @return 全件検索した受講生情報の一覧
+   */
   @Select("SELECT * FROM students")
   List<Student> selectAllStudentList();
 }
