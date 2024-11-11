4b00bce7cff294157e9a6c278c3775c77514aec9
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-11 22:12:53 +0900

    change: StudentCourseオブジェクトが持つフィールドstartDateとendDateの型定義を修正しました

--- src/main/java/portfolio/StudentManagement/data/StudentCourse.java
+++ src/main/java/portfolio/StudentManagement/data/StudentCourse.java
@@ -1,6 +1,7 @@
 package portfolio.StudentManagement.data;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,6 @@ public class StudentCourse {
   private String id;
   private String studentId;
   private String courseName;
-  private Timestamp startDate;
-  private Timestamp endDate;
+  private LocalDateTime startDate;
+  private LocalDateTime endDate;
 }
