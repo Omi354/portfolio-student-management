68cb78712b8c15dbe44d1a673dfed7ed681b84d0
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-19 22:27:06 +0900

    refactor: GETメソッドとそれに関連するServiceでの処理についてリファクタリングを行いました

--- src/main/java/portfolio/StudentManagement/controller/StudentController.java
+++ src/main/java/portfolio/StudentManagement/controller/StudentController.java
@@ -9,40 +9,48 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import portfolio.StudentManagement.controller.converter.StudentConverter;
-import portfolio.StudentManagement.data.Student;
-import portfolio.StudentManagement.data.StudentCourse;
 import portfolio.StudentManagement.domain.StudentDetail;
 import portfolio.StudentManagement.service.StudentService;
 
+/**
+ * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです
+ */
 @RestController
 public class StudentController {
 
   private StudentService service;
-  private StudentConverter converter;
 
+  /**
+   * コンストラクタ
+   *
+   * @param service 　受講生サービス
+   */
   @Autowired
-  public StudentController(StudentService service, StudentConverter converter) {
+  public StudentController(StudentService service) {
     this.service = service;
-    this.converter = converter;
   }
 
+  /**
+   * 受講生詳細一覧の全件検索です。 全件検索を行うので、条件指定は行いません。
+   *
+   * @return 受講生詳細一覧（全件）
+   */
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList() {
-    List<Student> allStudentList = service.getAllStudentList();
-    List<StudentCourse> allStudentCourseList = service.getAllStudentCourseList();
-    return converter.getStudentDetailsList(allStudentList, allStudentCourseList);
+    return service.getAllStudentDetailList();
   }
 
+  /**
+   * 受講生検索です IDに紐づく任意の受講生の情報を取得します
+   *
+   * @param id 受講生id
+   * @return idに紐づく任意の受講生情報
+   */
   @GetMapping("/student/{id}")
   public StudentDetail getStudent(@PathVariable String id) {
     return service.getStudentDetailById(id);
   }
 
-  @GetMapping("/studentCourseList")
-  public List<StudentCourse> getStudentCourseList() {
-    return service.getAllStudentCourseList();
-  }
 
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
--- src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
+++ src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
@@ -11,22 +11,29 @@ import portfolio.StudentManagement.data.Student;
 import portfolio.StudentManagement.data.StudentCourse;
 
 /**
- * 受講生コース情報を扱うリポジトリ 全件検索や単一条件での検索が行えるクラスです
+ * 受講生コーステーブルを扱うリポジトリです。
  */
 @Mapper
 public interface StudentCourseRepository {
 
   /**
-   * 全件検索します
+   * 受講生コース情報の全件検索を行います。
    *
-   * @return 全件検索した受講生コースの一覧
+   * @return 受講生コース一覧（全件）
    */
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> selectAllCourseList();
 
+  /**
+   * 受講生コース情報の検索を行います。 任意の受講生のIDを指定し、任意の受講生に紐づく受講生コース情報を検索します。
+   *
+   * @param studentId 受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> selectCourseListByStudentId(String studentId);
 
+  
   @Insert("INSERT INTO students_courses (student_id, course_name, start_date, end_date) VALUES((SELECT id FROM students WHERE email = #{student.email}), #{studentCourse.courseName}, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 YEAR))")
   @Options(useGeneratedKeys = true, keyProperty = "studentCourse.id", keyColumn = "id")
   void createStudentCourse(@Param("student") Student student,
