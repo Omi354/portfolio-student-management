414d91602c2501b1116bd5345b6a9bef1496f3b4
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-12-01 13:45:29 +0900

    test: 受講生登録のテストを作成しました

--- src/test/java/portfolio/StudentManagement/repository/StudentRepositoryTest.java
+++ src/test/java/portfolio/StudentManagement/repository/StudentRepositoryTest.java
@@ -4,9 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import portfolio.StudentManagement.data.Student;
@@ -93,5 +95,67 @@ class StudentRepositoryTest {
     assertThat(actual.getIsDeleted()).isEqualTo(isDeleted);
   }
 
+  @ParameterizedTest
+  @MethodSource("provideNewStudents")
+  void 受講生登録_渡されたStudentオブジェクトのレコードがDBにINSERTされること(Student student) {
+    // 準備
+    String id = student.getId();
+    int recordCountBefore = sut.selectAllStudentList().size();
 
+    // 実行
+    sut.createStudent(student);
+    Student actual = sut.selectStudentById(id);
+    int recordCountAfter = sut.selectAllStudentList().size();
+
+    //検証
+    assertThat(actual.getId()).isEqualTo(student.getId());
+    assertThat(actual.getFullName()).isEqualTo(student.getFullName());
+    assertThat(actual.getKana()).isEqualTo(student.getKana());
+    assertThat(actual.getNickName()).isEqualTo(student.getNickName());
+    assertThat(actual.getEmail()).isEqualTo(student.getEmail());
+    assertThat(actual.getCity()).isEqualTo(student.getCity());
+    assertThat(actual.getAge()).isEqualTo(student.getAge());
+    assertThat(actual.getGender()).isEqualTo(student.getGender());
+    assertThat(actual.getRemark()).isEqualTo(student.getRemark());
+    assertThat(actual.getIsDeleted()).isEqualTo(student.getIsDeleted());
+    assertThat(recordCountAfter).isEqualTo(recordCountBefore + 1);
+  }
+
+  public static Stream<Student> provideNewStudents() {
+    return Stream.of(
+        new Student.StudentBuilder("中村 一郎", "ichiro.nakamura@example.com", "横浜", 29)
+            .kana("ナカムラ イチロウ")
+            .nickName("いっちゃん")
+            .gender(Student.Gender.Male)
+            .remark("優秀な開発者")
+            .build(),
+
+        new Student.StudentBuilder("松本 由美", "yumi.matsumoto@example.com", "神戸", 33)
+            .nickName("ゆみちゃん")
+            .gender(Student.Gender.Female)
+            .remark("分析力が高い")
+            .build(),
+
+        new Student.StudentBuilder("藤田 和男", "kazuo.fujita@example.com", "仙台", 40)
+            .kana("フジタ カズオ")
+            .gender(Student.Gender.Male)
+            .remark("リーダーシップ抜群")
+            .build(),
+
+        new Student.StudentBuilder("小林 さくら", "sakura.kobayashi@example.com", "広島", 21)
+            .kana("コバヤシ サクラ")
+            .nickName("さくちゃん")
+            .remark("将来有望な学生")
+            .build(),
+
+        new Student.StudentBuilder("山田 太一", "taichi.yamada@example.com", "京都", 26)
+            .kana("ヤマダ タイチ")
+            .nickName("たいちゃん")
+            .gender(Student.Gender.Male)
+            .build(),
+
+        new Student.StudentBuilder("石川 幸子", "sachiko.ishikawa@example.com", "名古屋", 37)
+            .build()
+    );
+  }
 }
