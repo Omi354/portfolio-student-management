6459cdf8777ade6a8955f1bdffc5905e8c1ffe8d
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-12-03 02:39:03 +0900

    test: StudentRepositoryのテスト内容をインスタンス同士の比較ができるように修正しました

--- src/test/java/portfolio/StudentManagement/repository/StudentRepositoryTest.java
+++ src/test/java/portfolio/StudentManagement/repository/StudentRepositoryTest.java
@@ -7,12 +7,10 @@ import java.util.UUID;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import portfolio.StudentManagement.data.Student;
-import portfolio.StudentManagement.data.Student.Gender;
 
 @MybatisTest
 class StudentRepositoryTest {
@@ -24,22 +22,17 @@ class StudentRepositoryTest {
   @Test
   void 受講生全件検索_削除フラグがfalseの受講生情報を全件取得できること() {
     // 準備
-    String id1 = "1c91a1b0-1111-1111-1111-111111111111";
-    String id2 = "2c92b2c0-2222-2222-2222-222222222222";
-    String id3 = "3c93c3d0-3333-3333-3333-333333333333";
-    String id4 = "4c94d4e0-4444-4444-4444-444444444444";
-    String id5 = "5c95e5f0-5555-5555-5555-555555555555";
+    List<Student> expected = provideExistingStudents().toList();
 
     // 実行
     List<Student> actual = sut.selectAllStudentList();
 
     // 検証
-    assertThat(actual.size()).isEqualTo(5);
-    assertThat(actual.get(0).getId()).isEqualTo(id1);
-    assertThat(actual.get(1).getId()).isEqualTo(id2);
-    assertThat(actual.get(2).getId()).isEqualTo(id3);
-    assertThat(actual.get(3).getId()).isEqualTo(id4);
-    assertThat(actual.get(4).getId()).isEqualTo(id5);
+    assertThat(actual)
+        .hasSize(5)
+        .usingRecursiveFieldByFieldElementComparator()
+        .isEqualTo(expected);
+
   }
 
   @Test
@@ -55,36 +48,17 @@ class StudentRepositoryTest {
   }
 
   @ParameterizedTest
-  @CsvSource({
-      "'1c91a1b0-1111-1111-1111-111111111111', '佐藤 太郎', 'サトウ タロウ', 'たろちゃん', 'taro.sato@example.com', '東京', 25, 'Male', '優秀な学生です', false",
-      "'2c92b2c0-2222-2222-2222-222222222222', '鈴木 花子', 'スズキ ハナコ', 'はなちゃん', 'hanako.suzuki@example.com', '大阪', 28, 'Female', 'クリエイティブ志向', false",
-      "'3c93c3d0-3333-3333-3333-333333333333', '高橋 健一', 'タカハシ ケンイチ', NULL, 'kenichi.takahashi@example.com', '名古屋', 30, 'Male', 'リーダーシップあり', false",
-      "'4c94d4e0-4444-4444-4444-444444444444', '田中 美香', 'タナカ ミカ', 'みかりん', 'mika.tanaka@example.com', '福岡', 22, 'Female', '向上心が強い', false",
-      "'5c95e5f0-5555-5555-5555-555555555555', '山本 大輔', 'ヤマモト ダイスケ', NULL, 'daisuke.yamamoto@example.com', '札幌', 27, 'Male', '努力家', false",
-      "'6c96f6g0-6666-6666-6666-666666666666', '伊藤 次郎', 'イトウ ジロウ', 'ジロちゃん', 'jiro.ito@example.com', '仙台', 35, 'Male', '退会済みの学生', true"
-  })
-  void 受講生検索_適切なIDが渡された場合_IDに紐づく学生の情報を取得できること(
-      String id, String fullName, String kana, String nickName,
-      String email, String city, int age, Gender gender,
-      String remark, boolean isDeleted) {
-
-    // 準備
-    String actualNickName = nickName.equals("NULL") ? null : nickName;
+  @MethodSource("provideExistingStudents")
+  void 受講生検索_適切なIDが渡された場合_IDに紐づく学生の情報を取得できること(Student expected) {
+    String id = expected.getId();
 
     // 実行
     Student actual = sut.selectStudentById(id);
 
     // 検証
-    assertThat(actual.getId()).isEqualTo(id);
-    assertThat(actual.getFullName()).isEqualTo(fullName);
-    assertThat(actual.getKana()).isEqualTo(kana);
-    assertThat(actual.getNickName()).isEqualTo(actualNickName);
-    assertThat(actual.getEmail()).isEqualTo(email);
-    assertThat(actual.getCity()).isEqualTo(city);
-    assertThat(actual.getAge()).isEqualTo(age);
-    assertThat(actual.getGender()).isEqualTo(gender);
-    assertThat(actual.getRemark()).isEqualTo(remark);
-    assertThat(actual.getIsDeleted()).isEqualTo(isDeleted);
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .isEqualTo(expected);
   }
 
   @ParameterizedTest
@@ -100,40 +74,18 @@ class StudentRepositoryTest {
     int recordCountAfter = sut.selectAllStudentList().size();
 
     //検証
-    assertThat(actual.getId()).isEqualTo(student.getId());
-    assertThat(actual.getFullName()).isEqualTo(student.getFullName());
-    assertThat(actual.getKana()).isEqualTo(student.getKana());
-    assertThat(actual.getNickName()).isEqualTo(student.getNickName());
-    assertThat(actual.getEmail()).isEqualTo(student.getEmail());
-    assertThat(actual.getCity()).isEqualTo(student.getCity());
-    assertThat(actual.getAge()).isEqualTo(student.getAge());
-    assertThat(actual.getGender()).isEqualTo(student.getGender());
-    assertThat(actual.getRemark()).isEqualTo(student.getRemark());
-    assertThat(actual.getIsDeleted()).isEqualTo(student.getIsDeleted());
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .isEqualTo(student);
     assertThat(recordCountAfter).isEqualTo(recordCountBefore + 1);
   }
 
   @ParameterizedTest
-  @CsvSource({
-      "'1c91a1b0-1111-1111-1111-111111111111', '斎藤 太郎', 'サイトウ タロウ', 'たろ', 'taro.saito@example.com', '埼玉県草加市', 28, 'NON_BINARY', '優秀な社会人です', false",
-      "'2c92b2c0-2222-2222-2222-222222222222', '鈴木 花', 'スズキ ハナ', 'はなちゃん', 'hanako.suzuki@example.com', '大阪', 28, 'Female', 'クリエイティブ志向', false",
-      "'3c93c3d0-3333-3333-3333-333333333333', '高橋 健一', 'タカハシ ケンイチ', NULL, 'kenichi.takahashi@example.co.jp', '愛知県名古屋', 30, 'Male', 'リーダーシップあり', false",
-      "'4c94d4e0-4444-4444-4444-444444444444', '田中 美香', 'タナカ ミカ', 'みか', 'mika.tanaka@example.com', '福岡県福岡市', 21, 'Male', '向上心が強い', false",
-      "'5c95e5f0-5555-5555-5555-555555555555', '山本 大輔', 'ヤマモト ダイスケ', NULL, 'daisuke.yamamoto@example.com', '札幌', 27, 'Male', '疲れがち', false",
-      "'6c96f6g0-6666-6666-6666-666666666666', '伊藤 次郎', 'イトウ ジロウ', 'ジロちゃん', 'jiro.ito@example.com', '仙台', 35, 'Male', '退会済みの学生', true"
-  })
-  void 受講生更新_渡されたStudentオブジェクトのレコードでDBがUPDATEされること(
-      String id, String fullName, String kana, String nickName,
-      String email, String city, int age, Gender gender,
-      String remark, boolean isDeleted) {
+  @MethodSource("provideUpdatedStudents")
+  void 受講生更新_渡されたStudentオブジェクトのレコードでDBがUPDATEされること(Student student) {
 
     // 準備
-    String actualNickName = nickName.equals("NULL") ? null : nickName;
-
-    Student student = new Student.StudentBuilder(fullName, email, city, age)
-        .kana(kana).nickName(actualNickName).gender(gender).remark(remark)
-        .isDeleted(isDeleted).useOnlyTestBuildWithId(id);
-
+    String id = student.getId();
     int recordCountBefore = sut.selectAllStudentList().size();
 
     // 実行
@@ -142,18 +94,49 @@ class StudentRepositoryTest {
     int recordCountAfter = sut.selectAllStudentList().size();
 
     // 検証
-    assertThat(actual.getId()).isEqualTo(id);
-    assertThat(actual.getFullName()).isEqualTo(fullName);
-    assertThat(actual.getKana()).isEqualTo(kana);
-    assertThat(actual.getNickName()).isEqualTo(actualNickName);
-    assertThat(actual.getEmail()).isEqualTo(email);
-    assertThat(actual.getCity()).isEqualTo(city);
-    assertThat(actual.getAge()).isEqualTo(age);
-    assertThat(actual.getGender()).isEqualTo(gender);
-    assertThat(actual.getRemark()).isEqualTo(remark);
-    assertThat(actual.getIsDeleted()).isEqualTo(isDeleted);
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .isEqualTo(student);
     assertThat(recordCountAfter).isEqualTo(recordCountBefore);
 
+  }
+
+  public static Stream<Student> provideExistingStudents() {
+    return Stream.of(
+        new Student.StudentBuilder("佐藤 太郎", "taro.sato@example.com", "東京", 25)
+            .kana("サトウ タロウ")
+            .nickName("たろちゃん")
+            .gender(Student.Gender.Male)
+            .remark("優秀な学生です")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("1c91a1b0-1111-1111-1111-111111111111"),
+        new Student.StudentBuilder("鈴木 花子", "hanako.suzuki@example.com", "大阪", 28)
+            .kana("スズキ ハナコ")
+            .nickName("はなちゃん")
+            .gender(Student.Gender.Female)
+            .remark("クリエイティブ志向")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("2c92b2c0-2222-2222-2222-222222222222"),
+        new Student.StudentBuilder("高橋 健一", "kenichi.takahashi@example.com", "名古屋", 30)
+            .kana("タカハシ ケンイチ")
+            .gender(Student.Gender.Male)
+            .remark("リーダーシップあり")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("3c93c3d0-3333-3333-3333-333333333333"),
+        new Student.StudentBuilder("田中 美香", "mika.tanaka@example.com", "福岡", 22)
+            .kana("タナカ ミカ")
+            .nickName("みかりん")
+            .gender(Student.Gender.Female)
+            .remark("向上心が強い")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("4c94d4e0-4444-4444-4444-444444444444"),
+        new Student.StudentBuilder("山本 大輔", "daisuke.yamamoto@example.com", "札幌", 27)
+            .kana("ヤマモト ダイスケ")
+            .gender(Student.Gender.Male)
+            .remark("努力家")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("5c95e5f0-5555-5555-5555-555555555555")
+    );
   }
 
   public static Stream<Student> provideNewStudents() {
@@ -193,4 +176,51 @@ class StudentRepositoryTest {
             .build()
     );
   }
+
+  public static Stream<Student> provideUpdatedStudents() {
+    return Stream.of(
+        new Student.StudentBuilder("斎藤 太郎", "taro.saito@example.com", "埼玉県草加市", 28)
+            .kana("サイトウ タロウ")
+            .nickName("たろ")
+            .gender(Student.Gender.NON_BINARY)
+            .remark("優秀な社会人です")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("1c91a1b0-1111-1111-1111-111111111111"),
+        new Student.StudentBuilder("鈴木 花", "hanako.suzuki@example.com", "大阪", 28)
+            .kana("スズキ ハナ")
+            .nickName("はなちゃん")
+            .gender(Student.Gender.Female)
+            .remark("クリエイティブ志向")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("2c92b2c0-2222-2222-2222-222222222222"),
+        new Student.StudentBuilder("高橋 健一", "kenichi.takahashi@example.co.jp", "愛知県名古屋",
+            30)
+            .kana("タカハシ ケンイチ")
+            .gender(Student.Gender.Male)
+            .remark("リーダーシップあり")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("3c93c3d0-3333-3333-3333-333333333333"),
+        new Student.StudentBuilder("田中 美香", "mika.tanaka@example.com", "福岡県福岡市", 21)
+            .kana("タナカ ミカ")
+            .nickName("みか")
+            .gender(Student.Gender.Male)
+            .remark("向上心が強い")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("4c94d4e0-4444-4444-4444-444444444444"),
+        new Student.StudentBuilder("山本 大輔", "daisuke.yamamoto@example.com", "札幌", 27)
+            .kana("ヤマモト ダイスケ")
+            .gender(Student.Gender.Male)
+            .remark("疲れがち")
+            .isDeleted(false)
+            .useOnlyTestBuildWithId("5c95e5f0-5555-5555-5555-555555555555"),
+        new Student.StudentBuilder("伊藤 次郎", "jiro.ito@example.com", "仙台", 35)
+            .kana("イトウ ジロウ")
+            .nickName("ジロちゃん")
+            .gender(Student.Gender.Male)
+            .remark("退会済みの学生")
+            .isDeleted(true)
+            .useOnlyTestBuildWithId("6c96f6g0-6666-6666-6666-666666666666")
+    );
+  }
+
 }
