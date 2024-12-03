df581258c6253e11d11b1fc7b3082dbba723e344
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-12-04 06:22:21 +0900

    add: 申込状況を扱うためのRepositoryファイルを作成し、全件取得のメソッドを定義しました

---
+++ src/main/java/portfolio/StudentManagement/repository/EnrollmentStatusRepository.java
@@ -0,0 +1,14 @@
+package portfolio.StudentManagement.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import portfolio.StudentManagement.data.EnrollmentStatus;
+
+@Mapper
+public interface EnrollmentStatusRepository {
+
+  @Select("SELECT * FROM enrollment_statuses")
+  List<EnrollmentStatus> selectAllEnrollmentStatus();
+
+}
