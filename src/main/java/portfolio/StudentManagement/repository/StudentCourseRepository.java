1523c17b182a682007356829e6137b5dee4c29f9
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-12-28 23:10:24 +0900

    feat: DB層に受講生コース条件検索用のメソッドとSQLを追加しました

--- src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
+++ src/main/java/portfolio/StudentManagement/repository/StudentCourseRepository.java
@@ -1,7 +1,9 @@
 package portfolio.StudentManagement.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import portfolio.StudentManagement.data.EnrollmentStatus.Status;
 import portfolio.StudentManagement.data.StudentCourse;
 
@@ -19,13 +21,28 @@ public interface StudentCourseRepository {
   List<StudentCourse> selectAllCourseList();
 
   /**
-   * 受講生コース情報の検索を行います。任意の受講生のIDを指定し、任意の受講生に紐づく受講生コース情報を検索します。
+   * 受講生コース情報のID検索を行います。任意の受講生のIDを指定し、任意の受講生に紐づく受講生コース情報を検索します。
    * コースの申込状況については最新のものを取得し、受講生コース情報のフィールドに含めて返します。
    *
    * @param studentId 受講生ID
    * @return 受講生IDに紐づく受講生コース情報
    */
   List<StudentCourse> selectCourseListByStudentId(String studentId);
+
+  /**
+   * 受講生コース情報の条件検索を行います。コース名・受講開始日・受講修了日を指定し、該当の申込状況に合致するレコードを返します。 条件の指定がない場合には、全件を返します。
+   * 申込状況については最新のものを取得し、受講生コース情報のフィールドに含めて返します。
+   *
+   * @param courseName 受講コース名
+   * @param startDate  受講開始日
+   * @param endDate    受講終了日
+   * @return 検索条件に合致する受講生コース情報のリスト
+   */
+  List<StudentCourse> selectCourseListBySearchQuery(
+      @Param("courseName") String courseName,
+      @Param("startDate") LocalDateTime startDate,
+      @Param("endDate") LocalDateTime endDate
+  );
 
   /**
    * 受講生コース情報の申込状況検索を行います。申込状況を指定し、該当の申込状況に合致するレコードを返します。
