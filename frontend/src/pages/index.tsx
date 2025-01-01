deee5cfd5f5505208079524deb48f6ff95512af1
Omi <bell.tsaeb.shoji+work@gmail.com>
2025-01-02 06:35:44 +0900

    feat: 受講生一覧の絞り込み機能を実装しました

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -9,8 +9,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Stack,
+  TextField,
+  Button,
 } from '@mui/material'
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { fetcher } from '@/utils'
 
@@ -42,11 +46,103 @@ type StudentProps = {
 const StudentPage: NextPage = () => {
   const url = 'http://localhost:8080/students'
   const { data, error } = useSWR(url, fetcher)
+  const [fullName, setFullName] = useState('')
+  const [kana, setKana] = useState('')
+  const [nickName, setNickName] = useState('')
+  const [email, setEmail] = useState('')
+  const [city, setCity] = useState('')
+  const [maxAge, setMaxAge] = useState('')
+  const [minAge, setMinAge] = useState('')
+  const [gender, setGender] = useState('')
+  const [remark, setRemark] = useState('')
+  const [fillteredData, setFilteredData] = useState<StudentProps[]>([])
+
+  useEffect(() => {
+    if (data) {
+      let result = data
+
+      if (fullName) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.fullName.includes(fullName),
+        )
+      }
+      if (kana) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.kana.includes(kana),
+        )
+      }
+      if (nickName) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.nickName.includes(nickName),
+        )
+      }
+      if (email) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.email.includes(email),
+        )
+      }
+      if (city) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.city.includes(city),
+        )
+      }
+      if (gender) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.gender.includes(gender),
+        )
+      }
+      if (remark) {
+        result = result.filter((studentData: StudentProps) =>
+          studentData.student.remark.includes(remark),
+        )
+      }
+      if (maxAge) {
+        result = result.filter(
+          (studentData: StudentProps) =>
+            studentData.student.age <= Number(maxAge),
+        )
+      }
+      if (minAge) {
+        result = result.filter(
+          (studentData: StudentProps) =>
+            studentData.student.age >= Number(minAge),
+        )
+      }
+      setFilteredData(result)
+    }
+  }, [
+    data,
+    fullName,
+    kana,
+    nickName,
+    email,
+    city,
+    maxAge,
+    minAge,
+    gender,
+    remark,
+  ])
 
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
 
-  const studentsData = data
+  const handleInputChange =
+    (setter: React.Dispatch<React.SetStateAction<string>>) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setter(event.target.value)
+    }
+
+  const handleReset = () => {
+    setFullName('')
+    setKana('')
+    setNickName('')
+    setEmail('')
+    setCity('')
+    setMaxAge('')
+    setMinAge('')
+    setGender('')
+    setRemark('')
+  }
 
   return (
     <Box sx={{ backgroundColor: '#f9f9f9', minHeight: '100vh', py: 4 }}>
@@ -54,6 +150,69 @@ const StudentPage: NextPage = () => {
         <Typography variant="h4" gutterBottom>
           受講生一覧
         </Typography>
+
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="氏名"
+            value={fullName}
+            variant="outlined"
+            onChange={handleInputChange(setFullName)}
+          />
+          <TextField
+            label="カナ名"
+            value={kana}
+            variant="outlined"
+            onChange={handleInputChange(setKana)}
+          />
+          <TextField
+            label="ニックネーム"
+            value={nickName}
+            variant="outlined"
+            onChange={handleInputChange(setNickName)}
+          />
+          <TextField
+            label="メールアドレス"
+            value={email}
+            variant="outlined"
+            onChange={handleInputChange(setEmail)}
+          />
+          <TextField
+            label="居住地域"
+            value={city}
+            variant="outlined"
+            onChange={handleInputChange(setCity)}
+          />
+        </Stack>
+
+        <Stack direction="row" spacing={2} sx={{ mt: 1, mb: 1 }}>
+          <TextField
+            label="最大年齢"
+            value={maxAge}
+            variant="outlined"
+            onChange={handleInputChange(setMaxAge)}
+          />
+          <TextField
+            label="最小年齢"
+            value={minAge}
+            variant="outlined"
+            onChange={handleInputChange(setMinAge)}
+          />
+          <TextField
+            label="性別"
+            value={gender}
+            variant="outlined"
+            onChange={handleInputChange(setGender)}
+          />
+          <TextField
+            label="備考"
+            value={remark}
+            variant="outlined"
+            onChange={handleInputChange(setRemark)}
+          />
+          <Button variant="contained" onClick={handleReset}>
+            リセット
+          </Button>
+        </Stack>
 
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -70,7 +229,7 @@ const StudentPage: NextPage = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {studentsData.map((studentData: StudentProps) => (
+              {fillteredData.map((studentData: StudentProps) => (
                 <TableRow
                   key={studentData.student.id}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
