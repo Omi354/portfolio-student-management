5c006bd72bd5bb1a726110b37085f2485e20be45
Omi <bell.tsaeb.shoji+work@gmail.com>
2025-01-06 00:20:26 +0900

    chore: バックエンドのエンドポイントに/apiをつけました

--- backend/src/main/java/portfolio/StudentManagement/controller/StudentController.java
+++ backend/src/main/java/portfolio/StudentManagement/controller/StudentController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import portfolio.StudentManagement.data.EnrollmentStatus;
@@ -40,6 +41,7 @@ import portfolio.StudentManagement.service.StudentService;
  */
 @Validated
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
